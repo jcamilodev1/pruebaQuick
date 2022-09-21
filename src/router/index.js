23e3964609148e171d4ac98967bd267cbab4f209
camilo garcia <camilo456garcia@gmail.com>
2022-09-20 20:13:05 -0500

    changing auth data and base

--- src/router/index.js
+++ src/router/index.js
@@ -2,13 +2,23 @@ import { createRouter, createWebHistory } from "vue-router";
 
 const routes = [
   {
-    path: '/',
-    component: () => import('@/layouts/Auth.vue'),
+    path: "",
+    component: () => import("@/layouts/Main.vue"),
     children: [
       {
-        path: '/login',
-        name:'Login',
-        component: () => import ('@/views/auth/Index.vue')
+        path: "/",
+        name: "Dashboard",
+        component: () => import("@/views/dashboard/Index.vue"),
+      },
+    ],
+  },
+  {
+    component: () => import("@/layouts/Auth.vue"),
+    children: [
+      {
+        path: "/login",
+        name: "Login",
+        component: () => import("@/views/auth/Index.vue"),
       },
     ],
   },
@@ -18,5 +28,14 @@ const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
 });
+const existToken = () => {
+  return true
+  return !!localStorage.token;
+}
 
+router.beforeEach((to, from, next) => {
+  if (to.name !== 'Login' && existToken()) next({ name: 'Login' })
+  // if the user is not authenticated, `next` is called twice
+  next()
+});
 export default router;
