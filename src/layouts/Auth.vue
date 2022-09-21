8fe21ca2b7a7436e5912b57e6ce87c133a0e6c20
camilo garcia <camilo456garcia@gmail.com>
2022-09-20 19:48:08 -0500

    loging and register

---
+++ src/layouts/Auth.vue
@@ -0,0 +1,55 @@
+<template>
+  <section class="authLayout">
+    <article class="authLayout__img">
+      <img src="https://images.pexels.com/photos/3802666/pexels-photo-3802666.jpeg?cs=srgb&dl=pexels-will-mu-3802666.jpg&fm=jpg" alt="">
+    </article>
+    <article class="authLayout__contain">
+        <img src="/src/assets/img/logoAzul.png" alt="">
+        <router-view />
+    </article>
+  </section>
+    
+</template>
+
+<script>
+
+export default {
+};
+</script>
+
+<style lang="scss" scoped>
+  .authLayout{
+    height: 100vh;
+    overflow: hidden;
+    display: grid;
+    grid-template-columns: 60% 40%;
+    @media (max-width: 780px){
+      height: auto;
+      grid-template-columns: 1fr;
+    }
+    &__img{
+      @media (max-width: 780px){
+        display: none;
+      }
+      img{
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+      }
+    }
+    &__contain{
+      padding-top: 80px;
+      text-align: center;
+      background-color: white;
+      @media (max-width: 780px){
+        padding-top: 0px;
+      }
+      img{
+        max-width: 200px;
+        margin: 0 auto;
+      }
+    }
+  }
+</style>
+
+
---
+++ src/views/auth/Login.vue
@@ -0,0 +1,63 @@
+<template>
+  <section class="loginContainer">
+    <label for=""><span>*</span> Email</label>
+    <BaseInput placeholder="example@mail.com" v-model="form.email"></BaseInput>
+    <label for=""><span>*</span> Contraseña</label>
+    <InputPass placeholder="Password" v-model="form.pass"></InputPass>
+    <BaseButton label="Entrar"></BaseButton>
+    <div>
+      <router-link to="/about">Olvide mi clave</router-link>
+    </div>
+  </section>
+</template>
+
+<script>
+  
+  import BaseInput from '@/components/form/BaseInput.vue';
+import InputPass from '@/components/form/InputPass/InputPass.vue';
+import { ref } from '@vue/reactivity';
+import BaseButton from '../../components/form/BaseButton.vue';
+export default {
+    components: { BaseInput, InputPass, BaseButton },
+    setup(){
+      const form = ref({})
+      const login = () => {
+        
+      }
+      return{
+        form,
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+.loginContainer{
+  padding: 32px;
+  text-align: start;
+  label{
+    display: block;
+    font-size: 16px;
+    font-weight: 400;
+    margin-bottom: 10px;
+    color: #000;
+    span{
+      color: #FF4E78;
+    }
+  }
+  input{
+    margin-bottom: 25px;
+  }
+  a{
+    color: #fcb100;
+    font-size: 16px;
+    font-weight: 500;
+    &:hover{
+      text-decoration: none;
+    }
+  }
+  > div{
+    text-align: center;
+  }
+}
+</style>
