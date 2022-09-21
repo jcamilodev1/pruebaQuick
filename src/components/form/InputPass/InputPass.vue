63d4e165bcca90aad703dc1db8b68127389a2e01
camilo garcia <camilo456garcia@gmail.com>
2022-09-20 19:31:50 -0500

    pass input

---
+++ src/components/form/InputPass/InputPass.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="InputPass">
+    <BaseInput
+      :value="modelValue"
+      :placeholder="label"
+      v-bind="$attrs"
+      @input="$emit('update:modelValue', $event.target.value)"
+      :type="show"
+    ></BaseInput>
+    <img src="./EyeVisible.svg" alt="" @click="showpass" />
+  </div>
+</template>
+
+<script>
+import { ref } from '@vue/reactivity';
+import BaseInput from "../BaseInput.vue";
+export default {
+  components: { BaseInput },
+  props: {
+    label: {
+      type: String,
+      default: "",
+    },
+    modelValue: {
+      type: [String, Number],
+      default: "",
+    },
+  },
+  setup(){
+    const show =  ref("password")
+    const showpass = () => {
+      const type = {
+        "text" : "password",
+        "password" : "text"
+      }
+      show.value = type[show.value]
+    }
+    return{
+      show,
+      showpass
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.InputPass {
+  position: relative;
+  img {
+    position: absolute;
+    top: 50%;
+    left: 95%;
+    transform: translate(-95%, -50%);
+    cursor: pointer;
+  }
+}
+</style>
