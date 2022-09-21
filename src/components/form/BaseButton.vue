ad744511bb67144b43036ea5a863023b2dbc0bc7
camilo garcia <camilo456garcia@gmail.com>
2022-09-20 19:27:35 -0500

    base inputs

---
+++ src/components/form/BaseButton.vue
@@ -0,0 +1,35 @@
+<template>
+  <button>{{label}}</button>
+</template>
+
+<script>
+export default {
+  props: {
+    label: {
+      type: String,
+      default: "",
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+  button{
+    display: block;
+    width: 100%;
+    margin: 25px 0;
+    padding: 10px;
+    border-radius: 8px;
+    background: linear-gradient(180deg, #fcd6a4 0%, #fcb100 100%);
+    color: white;
+    font-weight: 600;
+    border: none;
+    font-size: 14px;
+    cursor: pointer;
+    transition: all 1s;
+    &:hover{
+      background: linear-gradient(180deg, #fcd6a485 0%, #fcb100 100%);
+      transition: all 1s;
+    }
+  }
+</style>
---
+++ src/components/form/BaseInput.vue
@@ -0,0 +1,40 @@
+<template>
+  <input
+    :value="modelValue"
+    :placeholder="label"
+    v-bind="$attrs"
+    @input="$emit('update:modelValue', $event.target.value)"
+  />
+</template>
+
+<script>
+export default {
+  props: {
+    label: {
+      type: String,
+      default: "",
+    },
+    modelValue: {
+      type: [String, Number],
+      default: "",
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+  input{
+    width: 100%;
+    padding: 14px;
+    border-radius: 8px;
+    color: #000626;
+    background-color: #F6F8FA;
+    border: none;
+    outline: none;
+    font-size: 14px;
+    &::placeholder{
+      font-weight: 300;
+      color: #7F8894;
+    }
+  }
+</style>
